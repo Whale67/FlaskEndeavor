30af9264c8d1b3896d0fdf7c5b586e85e677c0b9
aristotle3 <naomielisha@protonmail.com>
2017-07-09 21:06:13 -0400

    configs

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ from wtforms.validators import Length, Email
 
 app = Flask(__name__)
 
-app.config.from_my_envar('DEBUG')
-app.config.from_my_envar('CSRF_ENABLED')
-app.config.from_my_envar('SECRET_KEY')
-app.config.from_my_envar('FROM_EMAIL')
-app.config.from_my_envar('FROM_PASSWORD')
-app.config.from_my_envar('TO_EMAIL')
+app.config.from_envar('DEBUG')
+app.config.from_envar('CSRF_ENABLED')
+app.config.from_envar('SECRET_KEY')
+app.config.from_envar('FROM_EMAIL')
+app.config.from_envar('FROM_PASSWORD')
+app.config.from_envar('TO_EMAIL')
 
 class ContactForm(FlaskForm):
    firstName = TextField('First Name', validators=[
