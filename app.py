58edcd46c986e1cf65e4c0b6f51bd8f4a833e28b
aristotle3 <naomielisha@protonmail.com>
2017-07-09 21:15:13 -0400

    configs

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from wtforms.validators import Required, EqualTo, Optional
 from wtforms.validators import Length, Email
 
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 
 app.config.from_envvar('DEBUG')
 app.config.from_envvar('CSRF_ENABLED')
