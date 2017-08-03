6d5432377642ddc5750f2589312e4f80e17808c8
aristotle3 <naomielisha@protonmail.com>
2017-08-03 18:00:40 -0400

    changed styles

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function(){
 	$("h1").toggleClass("animated zoomIn");
 	$("span").toggleClass("animated zoomIn");
@@ -47,54 +45,4 @@ function delayURL(URL) {
 		window.location.href = URL
 	}, 1000);
 }
-
-//Contact Form
-
-$(document).ready(function(){
-	$('#contactForm').bootstrapValidator({
-		fields: {
-			firstName: {
-				validators:{
-					notEmpty: {
-						message: 'Hi, my name is Naomi! And you are...?'
-					}
-				}
-			},
-			lastName: {
-				validators: {
-					notEmpty: {
-						message: "You might as well give me your entire name while you are here"
-					}
-				}
-			},
-			email: {
-				validators: {
-					notEmpty: {
-						message: "Your email is required so I can respond. It will remain private and never be sold!"
-					},
-					emailAddress: {
-						message: "Ummm, I don't think that email is valid, so lets try again"
-					}
-				}
-			},
-			subject: {
-				validators: {
-					notEmpty: {
-						message: "A email without a subject is like a present without a bow"
-					}
-				}
-			},
-			comments: {
-				validators: {
-					notEmpty: {
-						message: "Tell me something so I can tell you something in return"
-					}
-				}
-			}
-		}
-	});
-});
-
-
-
 
